23b01f8664d8dee056aec8e9cfbf30894b0733ea
paulcushing <pcushing@gmail.com>
2021-10-22 15:00:45 -0600

    Fix postcss build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: ['./src/**/*.tsx'],
+    purge: [
+        './src/pages/**/*.{js,jsx,ts,tsx}',
+        './src/components/**/*.{js,jsx,ts,tsx}',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {},
     variants: {
